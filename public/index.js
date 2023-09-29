46ce06158cf16b1046717871f281e41d2eb67d10
sjcowdery96 <sjc.cowdery@gmail.com>
2023-09-29 11:12:29 -0500

    init commit

--- public/index.js
+++ public/index.js
@@ -1,8 +1,11 @@
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
+
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
 
 }
 
